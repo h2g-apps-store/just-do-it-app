171507c2a83ff01f3a647b5ac5d661f00907c927
inspecthers <kg.geku@gmail.com>
2023-04-09 21:16:25 +0100

    init projects

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Hiro } from "./components/Hiro";
+
+function App() {
+  return (
+    <>
+      <Hiro />
+      <main>
+        <ul>
+          <li>un</li>
+          <li>Deux</li>
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default App;
