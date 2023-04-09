5a74492bbbd3a4949c9f4dc2946d0fe03611a53c
inspecthers <kg.geku@gmail.com>
2023-04-09 21:58:19 +0100

    fix hiro

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import { Hiro } from "./components/Hiro";
 
 function App() {
   return (
-    <>
+    <div className="h-screen flex flex-col bg-gray-100">
       <Hiro />
-      <main>
-        <ul>
-          <li>un</li>
-          <li>Deux</li>
-        </ul>
-      </main>
-    </>
+      <div className="px-96 py-5 space-y-5 bg-gray-100 ">
+        <form action="#0">
+          <div className="flex gap-3 ">
+            <input
+              className="w-full py-1 px-2 shadow-sm h-12 text-xl  outline-none border border-[#059669] font-light text-gray-600 cursor-pointer active:border-[#75eac5] hover:border-[#75eac5]"
+              type="text"
+              name="task"
+              placeholder="Preciser la tache"
+            />
+            <button className="font-bold w-60 text-white p-0  flex items-center justify-center text-3xl bg-[#059669] hover:bg-[#75eac5] active:bg-[#059669] cursor-pointer hover:text-[#059669] active:text-white">
+              +
+            </button>
+          </div>
+        </form>
+        <div className="bg-red-600 w-auto">
+          <ul>
+            <li>un</li>
+            <li>Deux</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/Hiro.tsx
+++ src/components/Hiro.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Hiro: React.FC = () => {
   return (
-    <div className="w-full bg-[#059669] border-b-4 border-[#047857] h-40 sm:h-60 flex flex-col justify-center items-center sm:gap-3">
+    <div className="w-full bg-[#059669] border-b-4 border-[#047857] sm:h-60 flex flex-col justify-center items-center sm:gap-3">
       <h1 className="text-5xl sm:text-6xl font-bold text-white">JUST DO IT</h1>
       <p className="font-light text-xs sm:text-sm text-gray-200">
         THE TO DO APP.
