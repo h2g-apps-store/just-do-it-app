e58780548dfd1eacc4203de98cb7a164a60b1a70
inspecthers <kg.geku@gmail.com>
2023-04-11 00:06:32 +0100

    cons

--- src/components/ItemTask.tsx
+++ src/components/ItemTask.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FiEdit3 } from "react-icons/fi";
+import { TiDelete } from "react-icons/ti";
 
 export const ItemTask: React.FC = () => {
   return (
@@ -12,155 +14,12 @@ export const ItemTask: React.FC = () => {
           </p>
         </div>
         <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
+          <button title="Edit" className="text-blue-600 text-sm sm:text-lg">
+            <FiEdit3 />
           </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
+          <button title="Delete" className="text-red-500 text-sm sm:text-lg">
+            <TiDelete size={25} />
           </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
-        </div>
-      </li>
-      <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex items-center justify-center gap-3">
-          <input type="checkbox" className="w-5 h-5" />
-          <p className="text-gray-700 font-light text-sm sm:text-lg">
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet
-            consectetur
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="text-blue-600 text-sm sm:text-lg">
-            {/* <FiEdit3 /> */}
-            Edi
-          </button>
-          <button className="text-red-500 text-sm sm:text-lg">Del</button>
         </div>
       </li>
     </>
