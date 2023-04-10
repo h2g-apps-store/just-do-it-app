52f8164483de642bf98ae62a9356e89d4a21488a
inspecthers <kg.geku@gmail.com>
2023-04-10 21:29:43 +0100

    fix Hiro sections

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ function App() {
     <div className="h-screen flex flex-col bg-gray-100">
       <Hiro />
       <div className="px-96 py-5 space-y-5 bg-gray-100 ">
-        <form action="#0">
-          <div className="flex gap-3 ">
+        <form>
+          <div className="flex gap-3">
             <input
               className="w-full py-1 px-2 shadow-sm h-12 text-xl  outline-none border border-[#059669] font-light text-gray-600 cursor-pointer active:border-[#75eac5] hover:border-[#75eac5]"
               type="text"
               name="task"
               placeholder="Preciser la tache"
             />
-            <button className="font-bold w-60 text-white p-0  flex items-center justify-center text-3xl bg-[#059669] hover:bg-[#75eac5] active:bg-[#059669] cursor-pointer hover:text-[#059669] active:text-white">
+            <button
+              type="button"
+              className="font-bold w-60 text-white p-0  flex items-center justify-center text-3xl bg-[#059669] hover:bg-[#1fb888] active:bg-[#059669] cursor-pointer  active:text-white"
+            >
               +
             </button>
           </div>
--- src/components/ItemTask.tsx
+++ src/components/ItemTask.tsx
@@ -4,8 +4,8 @@ export const ItemTask: React.FC = () => {
   return (
     <>
       <li className="bg-gray-50 shadow-sm hover:bg-green-50 active:bg-green-100 cursor-pointer border border-gray-300 text-white p-2 text-xl flex justify-between items-center">
-        <div className="flex gap-3">
-          <input type="checkbox" />
+        <div className="flex items-center justify-center gap-3">
+          <input type="checkbox" className="w-4 h-4 " />
           <p className="text-gray-700 font-light text-lg">
             Lorem ipsum dolor sit amet consectetur{" "}
           </p>
